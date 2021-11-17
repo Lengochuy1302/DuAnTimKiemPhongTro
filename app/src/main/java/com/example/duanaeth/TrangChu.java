849d6966e8385d7ce05b94ccc8ac713c8ae4da33
Lê Ngọc Huy <Lengochuyc2130220002@gmail.com>
2021-11-17 20:34:06 +0700

    Add trang chủ

---
+++ app/src/main/java/com/example/duanaeth/TrangChu.java
@@ -0,0 +1,47 @@
+package com.example.duanaeth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class TrangChu extends AppCompatActivity {
+    public String linkRealTime;
+    TextView btnDangXuat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu);
+        //anh xạ
+        anhxa();
+        btnDangXuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                SweetAlertDialog pDialog = new SweetAlertDialog(TrangChu.this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading ...");
+                pDialog.setCancelable(true);
+                pDialog.show();
+                Intent introIntent = new Intent(TrangChu.this, IntroActivity.class);
+                startActivity(introIntent);
+                finishAffinity();
+            }
+        });
+    }
+
+    final private void anhxa() {
+        //gọi đường dẫn firebase
+        linkRealTime = getResources().getString(R.string.link_RealTime_Database);
+        //findViewById
+        btnDangXuat = findViewById(R.id.btndangxuat);
+
+    }
+}
--- app/src/main/java/com/example/duanaeth/UpdateProfile.java
+++ app/src/main/java/com/example/duanaeth/UpdateProfile.java
@@ -13,36 +13,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class UpdateProfile extends AppCompatActivity {
-    public String linkRealTime;
-    TextView btnDangXuat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_profile);
-        //anh xạ
-        anhxa();
-        btnDangXuat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                SweetAlertDialog pDialog = new SweetAlertDialog(UpdateProfile.this, SweetAlertDialog.PROGRESS_TYPE);
-                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
-                pDialog.setTitleText("Loading ...");
-                pDialog.setCancelable(true);
-                pDialog.show();
-                Intent introIntent = new Intent(UpdateProfile.this, IntroActivity.class);
-                startActivity(introIntent);
-                finishAffinity();
-            }
-        });
-    }
-
-    final private void anhxa() {
-        //gọi đường dẫn firebase
-        linkRealTime = getResources().getString(R.string.link_RealTime_Database);
-        //findViewById
-        btnDangXuat = findViewById(R.id.btndangxuat);
-
     }
 
 }
