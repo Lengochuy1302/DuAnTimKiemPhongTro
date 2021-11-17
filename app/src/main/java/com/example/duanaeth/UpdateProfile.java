d6957f7245ecfe5ea9053c8d6b71a60598faab54
Lê Ngọc Huy <Lengochuyc2130220002@gmail.com>
2021-11-17 22:27:30 +0700

    Add giao diện cập nhật

--- app/src/main/java/com/example/duanaeth/UpdateProfile.java
+++ app/src/main/java/com/example/duanaeth/UpdateProfile.java
@@ -13,11 +13,39 @@ import com.google.firebase.auth.FirebaseAuth;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class UpdateProfile extends AppCompatActivity {
-
+    public String linkRealTime;
+    TextView btnTiepTuc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_profile);
+        //ẩn toolBar
+        getSupportActionBar().hide();
+
+        //anh xạ
+        anhxa();
+        btnTiepTuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SweetAlertDialog pDialog = new SweetAlertDialog(UpdateProfile.this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading ...");
+                pDialog.setCancelable(true);
+                pDialog.show();
+                Intent introIntent = new Intent(UpdateProfile.this, TrangChu.class);
+                startActivity(introIntent);
+                finishAffinity();
+            }
+        });
     }
+
+    final private void anhxa() {
+        //gọi đường dẫn firebase
+        linkRealTime = getResources().getString(R.string.link_RealTime_Database);
+        //findViewById
+        btnTiepTuc = findViewById(R.id.btn_ok_capNhatActivity);
+
+    }
+
 
 }
