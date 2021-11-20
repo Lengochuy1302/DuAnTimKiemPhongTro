96582457dfdc2366bc3b19f373f8df93e137004a
tainguyenduy <nguyenduytai26112002@gmail.com>
2021-11-21 00:47:41 +0700

    add giao dien gioi thieu app

---
+++ app/src/main/java/com/example/duanaeth/thongtinphanmem.java
@@ -0,0 +1,15 @@
+package com.example.duanaeth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thongtinphanmem extends AppCompatActivity {
+     //thêm thông tin app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thongtimphanmem);
+        getSupportActionBar().hide();
+    }
+}
