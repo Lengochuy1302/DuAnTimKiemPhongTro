21b44a8437ae7679fe15089323db5330194e3d24
truonggiang2904 <dragon29042002@gmail.com>
2021-11-20 21:03:49 +0700

    add giao dien doi mk va quen mk

---
+++ app/src/main/java/com/example/duanaeth/doiMatKhauActivity.java
@@ -0,0 +1,15 @@
+package com.example.duanaeth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doiMatKhauActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doi_mat_khau);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/duanaeth/quenMatKhauActivity.java
@@ -0,0 +1,15 @@
+package com.example.duanaeth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quenMatKhauActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quen_mat_khau);
+        getSupportActionBar().hide();
+    }
+}
